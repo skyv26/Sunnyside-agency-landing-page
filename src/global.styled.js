26cee425331e3dbc854a287f8ecc37545a387fea
skyv26 <dev.aakashv@gmail.com>
2023-03-11 16:11:10 +0530

    Add the `global` styled component

---
+++ src/global.styled.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
+  
+  html {
+    font-size: 62.5%;
+    height: 100%;
+  }
+
+  body {
+    width: 100%;
+    min-height: 100%;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
