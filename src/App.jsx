9d04e8ab682d6bd01648ad78e087a35b7b004917
skyv26 <dev.aakashv@gmail.com>
2023-03-11 19:37:22 +0530

    Update the `App.jsx` file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import AgencyHeader from './components/header.styled';
+import SunnySideLogo from './utils/SunnySideLogo';
 
-const App = () => <h1>Aakash Verma</h1>;
+const App = () => (
+  <>
+    <AgencyHeader>
+     <SunnySideLogo />
+      <svg 
+        width="24" 
+        height="18"
+        role="img"
+        aria-labelledby='title desc'
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title id="title">Hamburger Icon</title>
+        <desc id="desc">Hamburger</desc>
+          <path d="M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z" 
+          fill="#FFF" 
+          fill-rule="evenodd"/>
+      </svg>
+    </AgencyHeader>
+  </>
+);
 
 export default App;
