dd55573e2b9f270da4f11e71b575b76ddc1fc578
skyv26 <dev.aakashv@gmail.com>
2023-03-13 10:30:59 +0530

    Add the media query for mobile response

--- src/components/HeroBanner/HeroBanner.styled.jsx
+++ src/components/HeroBanner/HeroBanner.styled.jsx
@@ -3,11 +3,12 @@ import MobileHeaderImage from '../../assets/images/mobile/image-header.jpg';
 
 export const HeroContainer = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
   height: 100vh;
   background: url(${MobileHeaderImage});
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: top center;
   background-size: cover;
   text-align: center;
   position: relative;
@@ -17,27 +18,31 @@ export const MainDisplayText = styled.h1`
   font-family: var(--fraunces);
   font-weight: 900;
   color: white;
-  font-size: 6.65vh;
+  font-size: 3rem;
   text-transform: uppercase;
   padding-top: 13rem;
   letter-spacing: 0.5rem;
+
+  @media (min-width: 23em) {
+    font-size: 4.25rem;
+  }
 `;
 
 export const ArrowButton = styled.img`
   display: inline-block;
   position: absolute;
-  top: 68%;
+  top: 70%;
   left: 50%;
-  transform: translate(-50%, -68%);
-  animation: downanimation 2s infinite 1s;
+  transform: translate(-50%, -70%);
+  animation: downanimation 3s infinite 1s;
   transition-delay: 0.25s ease-in;
 
   @keyframes downanimation {
     0% {
-      transform: translate(-50%, -68%);
+      transform: translate(-50%, -70%);
     }
     100% {
-      transform: translate(-50%, 4rem);
+      transform: translate(-50%, 5rem);
     }
   }
 `;
