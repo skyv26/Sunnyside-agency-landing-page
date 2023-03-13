8d1568928cc6f4b69aa9e2e471b97743aaec3b88
skyv26 <dev.aakashv@gmail.com>
2023-03-13 11:32:48 +0530

    Add the `feature` section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import HeroBanner from './components/HeroBanner/HeroBanner';
+import Feature from './components/Feature/Feature';
 
 const App = () => (
   <>
     <Header />
     <HeroBanner />
-    <HeroBanner />
+    <Feature />
   </>
 );
 
