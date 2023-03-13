3d7f8512ad07accc82f43e42081812d4ca731845
skyv26 <dev.aakashv@gmail.com>
2023-03-13 21:56:50 +0530

    Add the styles for Gallery

--- src/components/Interactive/Gallery.styled.jsx
+++ src/components/Interactive/Gallery.styled.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const GalleryContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 export const Image = styled.img`
+  display: inline-block;
 `;
