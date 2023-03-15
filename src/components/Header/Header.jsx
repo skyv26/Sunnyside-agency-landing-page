1d6864df1d70dc95c10aa9c7df9227f883123417
skyv26 <dev.aakashv@gmail.com>
2023-03-15 17:59:41 +0530

    Remove the commented code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,22 +15,6 @@ const Header = () => {
   return (
     <AgencyHeader>
       <SunnySideLogo />
-      {/* <svg
-        width="24"
-        height="18"
-        role="img"
-        aria-labelledby="title desc"
-        xmlns="http://www.w3.org/2000/svg"
-        onClick={hamburgerMenuHandler}
-      >
-        <title id="title">Hamburger Icon</title>
-        <desc id="desc">Hamburger</desc>
-        <path
-          d="M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z"
-          fill="#FFF"
-          fillRule="evenodd"
-        />
-      </svg> */}
       <Hamburger mobileMenuStatusChange={mobileMenuStatusChange} />
       {
         mobileMenuStatus && (
