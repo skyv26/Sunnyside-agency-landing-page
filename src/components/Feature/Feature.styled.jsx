ec92ea68736bd4ef8129bae4747fd02f096f229f
skyv26 <dev.aakashv@gmail.com>
2023-03-15 20:49:11 +0530

    Add the media query for laptop

--- src/components/Feature/Feature.styled.jsx
+++ src/components/Feature/Feature.styled.jsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import CherryImage from '../../assets/images/mobile/image-graphic-design.jpg';
+import OrangeImage from '../../assets/images/mobile/image-photography.jpg';
+import DesktopCherryImage from '../../assets/images/desktop/image-graphic-design.jpg';
+import DesktopOrangeImage from '../../assets/images/desktop/image-photography.jpg';
 
 export const FeatureSectionContainer = styled.div`
   width: 100%;
@@ -14,6 +18,14 @@ export const FeatureCardInner = styled(FeatureSectionContainer)`
   text-align: center;
   background-color: white;
   margin-top: -0.75rem;
+
+  @media (min-width: 48em) {
+    width: 50%;
+    padding: 0 7rem;
+    margin-top: 0;
+    text-align: left;
+    gap: 2rem;
+  }
 `;
 
 export const FeatureCardImage = styled.img`
@@ -21,25 +33,80 @@ export const FeatureCardImage = styled.img`
   width: 100%;
   object-fit: cover;
   object-position: top;
+
+  @media (min-width: 48em) {
+    width: 50%;
+  }
+`;
+
+export const ImageFeatureCardContainer = styled(FeatureSectionContainer)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 48em) {
+    flex-direction: row;
+  }
 `;
 
 export const FeatureCard = styled(FeatureSectionContainer)`
   width: 100%;
-  
-  &:nth-child(3),
-  &:nth-child(4) {
-    position: relative;
-    height: max-content;
-    margin-top: -0.75rem;
 
-    & ${FeatureCardImage} {
-      display: inline-block;
-      height: 100%;
+  @media (min-width: 48em) {
+    &:first-child, &:nth-child(2) {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    &:first-child {
+      flex-direction: row-reverse;
+    }
+  }
+
+  &.ImageCard {
+    position: relative;
+    margin-top: -0.75rem;
+    background: url(${CherryImage});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-origin: border-box;
+    background-position: center;
+    height: calc(100vh - 6rem);
+
+    &:last-child {
+      margin-top: -0.75rem;
+      background: url(${OrangeImage});
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-origin: border-box;
+      background-position: center;
+      height: calc(100vh - 6rem);
+    }
+
+    @media (min-width: 48em) {
+      width: 50%;
+      margin-top: 0;
+      background: url(${DesktopCherryImage});
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+      height: 32rem;
+
+      &:last-child {
+        margin-top: 0;
+        background: url(${DesktopOrangeImage});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-origin: border-box;
+        background-position: center;
+        height: 32rem;
+      }
     }
 
     & ${FeatureCardInner} {
-      position: absolute;
-      padding: 7rem 3.25rem;
+      padding: 0 3.25rem 7rem 3.25rem;
       width: 100%;
       gap: 3rem;
       height: 100%;
@@ -47,10 +114,32 @@ export const FeatureCard = styled(FeatureSectionContainer)`
       justify-content: flex-end;
       bottom: 0;
 
+      
+      @media (max-width: 20em) {
+        padding: 0 3.25rem 5rem 3.25rem;
+        gap: 2rem;
+      }
+
+      @media (min-width: 48em) {
+        text-align: center;
+        padding: 0 9rem 3rem 9rem;
+        gap: 2rem;
+      }
+
       & p {
         opacity: 0.7;
-        
-        @media (max-width: 360px) {
+
+        @media (max-width: 320px) {
+          &:first-child {
+            font-size: 2.25rem;
+          }
+
+          &:last-child {
+            font-size: 1.1rem;
+          }
+        }
+
+        @media (min-width: 22em) and (max-width: 360px) {
           &:first-child {
             font-size: 2.6rem;
           }
@@ -67,16 +156,24 @@ export const FeatureCard = styled(FeatureSectionContainer)`
 export const FeatureCardHeading = styled.p`
   font-family: var(--fraunces);
   font-weight: 900;
-  font-size: 3.6rem;
+  font-size: 3rem;
   color: hsl(212, 27%, 19%);
+
+  @media (min-width: 48em) {
+    font-size: 2.1rem;
+  }
 `;
 
 export const FeatureCardDesc = styled.p`
   font-family: var(--barlow);
   font-weight: 700;
-  font-size: 1.75rem;
+  font-size: 1.4rem;
   line-height: 1.6;
   color: hsl(213, 9%, 39%);
+
+  @media (min-width: 48em) {
+    font-size: 0.9rem;
+  }
 `;
 
 export const FeatureCardBtn = styled.a`
@@ -92,6 +189,12 @@ export const FeatureCardBtn = styled.a`
   width: max-content;
   margin: auto;
   cursor: pointer;
+
+  @media (min-width: 48em) {
+    font-size: 1.1rem;
+    text-align: left;
+    margin: 0;
+  }
   
   &::before {
     content: '';
@@ -105,6 +208,10 @@ export const FeatureCardBtn = styled.a`
     background-color: rgba(250, 212, 0, 0.4);
     z-index: -1;
     transition: background-color 0.5s ease-in-out;
+
+    @media (min-width: 48em) {
+      height: .4rem;
+    }
   }
   
   &.stand-out::before {
